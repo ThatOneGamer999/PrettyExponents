c9d18a55cbbe757d85a42a95f9967680f93f8f9d
ThatOneGamer999 <keenonsalish@icloud.com>
2016-12-11 09:02:04 -0800

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,41 @@
+while 1:
+ exp = int(raw_input('Power to? > '))
+ num = int(raw_input('Number? > '))
+ i = 0
+ num1 = 1
+ while i < exp:
+    
+    num1 = num1*num
+    i += 1
+    print num1
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ print ""
+ 
+ print 'Done.'
+ print num1
+ b = raw_input('Press Enter to exit')
